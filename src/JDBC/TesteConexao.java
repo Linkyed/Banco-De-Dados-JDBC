625caa7e592b5159d30b195576fe1d4ff8e726c7
Pedro Henrique A <phaalmeida1@gmail.com>
2023-03-30 16:14:14 -0300

    Conectar com Banco de Dados
    Usando a biblioteca do mysql connection JDBC para conectar com o banco de dados local MySQL

--- src/JDBC/TesteConexao.java
+++ src/JDBC/TesteConexao.java
@@ -1,7 +1,19 @@
 package JDBC;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class TesteConexao {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SQLException{
+		String url = "jdbc:mysql://localhost?verifyServerCertificate=false&useSSL=true";
+		String usuario = "root";
+		String senha = "1234567";
+		
+		Connection conexao = DriverManager.getConnection(url, usuario, senha);
+		
+		System.out.println("Conexão efetuado com sucesso!");
+		conexao.close();
 		
 	}
 }
--- src/module-info.java
+++ src/module-info.java
@@ -6,4 +6,5 @@
  *
  */
 module BancoDeDadosJDBC {
+	requires java.sql;
 }
